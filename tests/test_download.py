2b92359bb239672f0c73f8eab22d0348730d907d
Jesse Ward-Bond <jessewb.c@gmail.com>
2024-05-27 15:44:26 -0400

    Refactored to give return values, added tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,45 +2,105 @@ import unittest
 import warnings
 from pathlib import Path
 
+import geopandas as gpd
+
 import urbanity.download as ud
 
+import os
+import sys
 
-class TestDownloadOsmBoundary(unittest.TestCase):
+os.environ["GDAL_DATA"] = os.path.join(
+    f"{os.sep}".join(sys.executable.split(os.sep)[:-1]), "Library", "share", "gdal"
+)  # HACK
+
+# TODO add some test cases for coordinate systems
+
+
+class TestDownloadOSMBoundary(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.query = "East York, Ontario"
+        cls.boundarypath = Path("./tests/test_files/test_files_boundary.geojson")
+        cls.boundarypath.unlink(missing_ok=True)  # delete existing files
+
     def setUp(self) -> None:
-        warnings.simplefilter("ignore", category=DeprecationWarning)
+        warnings.simplefilter("ignore", category=DeprecationWarning)  # HACK
 
-    def test_download_osm_boundary_exists(self) -> None:
-        query = "East York, Ontario"
-        outpath = Path("./temp")
-        outpath.mkdir(exist_ok=True)
+    def test_download_osm_boundary(self) -> None:
+        query = self.query
+        boundarypath = self.boundarypath
 
-        ud.download_osm_boundary(query, outpath)
+        boundary = ud.download_osm_boundary(query, savepath=boundarypath.parent)
 
-        fileout = outpath / (outpath.stem + "_boundary.geojson")
+        # Test that the output file exists
+        self.assertTrue(boundarypath.exists())
 
-        self.assertTrue(fileout.exists())
+        # Test that the output and the return value are the same
+        gdf = gpd.read_file(boundarypath)
+        self.assertEqual(gdf.iloc[0]["geometry"], boundary)
 
     def test_download_osm_boundary_nonexistent(self):  # TODO
         pass
 
 
-class TestDownloadOsmNetwork(unittest.TestCase):
+class DownloadOSMNetwork(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        warnings.simplefilter("ignore", category=DeprecationWarning)  # HACK
+        cls.boundarypath = Path("./tests/test_files/test_files_boundary.geojson")
+        cls.boundarypath.unlink(missing_ok=True)
+
+        cls.boundary = ud.download_osm_boundary(
+            "East York, Toronto", savepath=cls.boundarypath.parent
+        )
+
     def setUp(self) -> None:
-        warnings.simplefilter("ignore", category=DeprecationWarning)
+        warnings.simplefilter("ignore", category=DeprecationWarning)  # HACK
 
-    def test_download_osm_network_exists(self) -> None:
-        query = "Kingston, Ontario"
-        outpath = Path("./temp")
-        outpath.mkdir(exist_ok=True)
+        self.networkpath = Path("./tests/test_files/test_files_road_network.geojson")
+        self.networkpath.unlink(missing_ok=True)  # delete existing files
 
-        ud.download_osm_network(query, outpath)
+    def test_download_from_file(self):
+        networkpath = self.networkpath
+        boundarypath = self.boundarypath
 
-        fileout = outpath / (outpath.stem + "_road_network.geojson")
+        network = ud.download_osm_network(boundarypath, savepath=networkpath.parent)
 
-        self.assertTrue(fileout.exists())
+        # Test that the output file exists
+        self.assertTrue(networkpath.exists())
 
-    def test_download_osm_boundary_nonexistent(self):  # TODO
-        pass
+    def test_download_from_polygon(self):
+        networkpath = self.networkpath
+        boundary = self.boundary
+
+        network = ud.download_osm_network(boundary, savepath=networkpath.parent)
+
+        # Test that the output file exists
+        self.assertTrue(networkpath.exists())
+
+    def test_download_error(self):
+        boundary = "East York, Toronto"
+
+        self.assertRaises(TypeError, ud.download_osm_network, boundary)
+
+
+# class TestDownloadOsmNetwork(unittest.TestCase):
+#     def setUp(self) -> None:
+#         warnings.simplefilter("ignore", category=DeprecationWarning)
+
+#     def test_download_osm_network_exists(self) -> None:
+#         query = "Kingston, Ontario"
+#         outpath = Path("./temp")
+#         outpath.mkdir(exist_ok=True)
+
+#         ud.download_osm_network(query, outpath)
+
+#         fileout = outpath / (outpath.stem + "_road_network.geojson")
+
+#         self.assertTrue(fileout.exists())
+
+#     def test_download_osm_boundary_nonexistent(self):  # TODO
+#         pass
 
 
 if __name__ == "__main__":
--- urbanity/download.py
+++ urbanity/download.py
@@ -3,6 +3,7 @@ from pathlib import Path, PurePath
 
 import shapely
 import osmnx as ox
+import geopandas as gpd
 
 import utils
 
@@ -12,46 +13,69 @@ import utils
 # TODO Query resolving, making sure they are all handled the same
 
 
-def download_osm_boundary(query: str, savepath: PurePath):
+def download_osm_boundary(query: str, savepath: PurePath = None):
     """Downloads the bounding polygon for the queried region using open streetmaps
 
-    The coordinate system is WGS84 / EPSG:4326
+    The coordinate system is WGS84 / EPSG:4326.
 
     Args:
         query (str): The region to geocode to get the bounding polygon. Can be place name or address]
-        savepath (pathlib.PurePath): Save location for downloaded polygon
+        savepath (pathlib.PurePath, optional): Save location for downloaded polygon. Defaults to None (not saving)
+
+    Returns:
+        shapely.geometry.polygon.Polygon: A shapely polygon representing the boundary of the queried region in EPSG:4326
     """
 
     print("Downloading boundaries...", end=" ")
     gdf_place = ox.geocoder.geocode_to_gdf(query)
     print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
+
     polygon = gdf_place["geometry"].unary_union
 
-    filepath = savepath / (savepath.stem + "_boundary.geojson")
-    filepath.write_text(shapely.to_geojson(polygon))
+    if savepath:
+        filepath = savepath / (savepath.stem + "_boundary.geojson")
+        filepath.write_text(shapely.to_geojson(polygon))
+
+    return polygon
 
 
-def download_osm_network(query: str, savepath: PurePath):
-    """Download a street network from open street map
+def download_osm_network(
+    polygon: PurePath | shapely.Polygon, savepath: PurePath = None
+):
+    """Download the street network within some polygon boundary
 
     The coordinate system is WGS84 / EPSG:4326
 
     Args:
         location (str): The region to geocode to get the road network. e.g. "Toronto, Ontario, Canada"
-        savepath (pathlib.PurePath): Save location for downloaded polygon
+        savepath (pathlib.PurePath, optional): Save location for downloaded polygon. Defaults to None (not saving)
+
+    Returns:
+        geojson: The road network as a geojson list of edges in WGS84 / EPSG:4326
     """
 
+    # Parse polygon if required
+    if isinstance(polygon, PurePath):
+        polygon = gpd.read_file(polygon).iloc[0]["geometry"]
+    elif not isinstance(polygon, shapely.Polygon):
+        raise TypeError(f"Expected shapely polygon or Path, got {type(polygon)}")
+
     # Get graph from OSM
-
     print("Downloading road network...", end=" ")
-    G = ox.graph_from_place(query, network_type="drive", simplify=False)
+    G = ox.graph_from_polygon(polygon, network_type="drive", simplify=False)
     print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
-    M = ox.convert.to_undirected(G)
 
+    M = ox.convert.to_undirected(G)
     _, gdf = ox.convert.graph_to_gdfs(M)
 
-    filepath = savepath / (savepath.stem + "_road_network.geojson")
-    filepath.write_text(gdf.to_json())
+    # Save and return
+    output = gdf.to_json()
+
+    if savepath:
+        filepath = savepath / (savepath.stem + "_road_network.geojson")
+        filepath.write_text(output)
+
+    return output
 
 
 def download_rsi(boundary_path: PurePath):  # TODO
