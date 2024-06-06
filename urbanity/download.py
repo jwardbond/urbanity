0c1c74701411c98e27fb8b3fc265d581d8a74eb6
jwardbond <jessewb.c@gmail.com>
2024-06-06 15:20:22 -0400

    Now downloads OSM buildinng footprints

--- urbanity/download.py
+++ urbanity/download.py
@@ -1,4 +1,6 @@
+import re
 import argparse
+import warnings
 from pathlib import Path, PurePath
 
 import shapely
@@ -25,8 +27,9 @@ def download_osm_boundary(query: str, savepath: PurePath = None):
     Returns:
         shapely.geometry.polygon.Polygon: A shapely polygon representing the boundary of the queried region in EPSG:4326
     """
+    print(savepath)
 
-    print("Downloading boundaries...", end=" ")
+    print(f"Downloading boundaries for {query} from OSM...", end=" ")
     gdf_place = ox.geocoder.geocode_to_gdf(query)
     print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
 
@@ -35,6 +38,8 @@ def download_osm_boundary(query: str, savepath: PurePath = None):
     if savepath:
         filepath = savepath / (savepath.stem + "_boundary.geojson")
         filepath.write_text(shapely.to_geojson(polygon))
+
+    print(savepath, 3)
 
     return polygon
 
@@ -55,13 +60,10 @@ def download_osm_network(
     """
 
     # Parse polygon if required
-    if isinstance(polygon, PurePath):
-        polygon = gpd.read_file(polygon).iloc[0]["geometry"]
-    elif not isinstance(polygon, shapely.Polygon):
-        raise TypeError(f"Expected shapely polygon or Path, got {type(polygon)}")
+    polygon = _parse_polygon(polygon)
 
     # Get graph from OSM
-    print("Downloading road network...", end=" ")
+    print("Downloading road network from OSM...", end=" ")
     G = ox.graph_from_polygon(polygon, network_type="drive", simplify=False)
     print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
 
@@ -76,6 +78,31 @@ def download_osm_network(
     return gdf
 
 
+def download_osm_buildings(
+    polygon: PurePath | shapely.Polygon, savepath: PurePath = None
+):
+    # Parse polygon if required
+    polygon = _parse_polygon(polygon)
+
+    # Get buildings from
+    print("Downloading buildings from OSM...", end=" ")
+
+    with warnings.catch_warnings():  # HACK geopandas warning suppression
+        warnings.simplefilter("ignore")
+        feats = ox.features_from_polygon(polygon, tags={"building": True})
+
+    print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
+
+    # Save and return
+    if savepath:
+        filepath = savepath / (savepath.stem + "osm_building_polygons.geojson")
+        filepath.write_text(feats.to_json())
+
+    print(feats.head(3))
+
+    return feats
+
+
 def download_rsi(boundary_path: PurePath):  # TODO
     pass
 
@@ -84,12 +111,47 @@ def download_buildings(boundary_path: PurePath):  # TODO
     pass
 
 
+def _place_to_filename(place: str):
+    """Given a string, strip all punctuation, lowercase, and replaces spaces with underscores"""
+    place = place.lower()
+    place = re.sub(r"[^\w\s]", "", place)
+    place = place.replace(" ", "_")
+    return place
+
+
+def _parse_polygon(polygon: PurePath | shapely.Polygon):
+    """Parses polygon representations to return a shapely polygon"""
+
+    if isinstance(polygon, PurePath):
+        polygon = gpd.read_file(polygon).iloc[0]["geometry"]
+    elif not isinstance(polygon, shapely.Polygon):
+        raise TypeError(f"Expected shapely polygon or Path, got {type(polygon)}")
+
+    return polygon
+
+
+if __name__ == "__mains__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("place")
+    args = parser.parse_args()
+    place = args.place
+
+    savepath = Path(f"results/{_place_to_filename(place)}")
+
+    savepath.mkdir(exist_ok=True, parents=True)
+
+    print(savepath / (savepath.stem + "_filename"))
+    download_osm_boundary(query=place, savepath=savepath)
+
+    download_osm_network(
+        polygon=savepath / (savepath.stem + "_boundary.geojson"),
+        savepath=savepath,
+    )
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("savepath")
+    parser.add_argument("boundarypath")
     args = parser.parse_args()
+    boundarypath = Path(args.boundarypath)
 
-    savepath = Path(args.savepath)
-    savepath.mkdir(exist_ok=True)
-
-    download_osm_network(savepath / (savepath.stem + "_boundary.geojson"), savepath)
+    download_osm_buildings(boundarypath, boundarypath.parents[0])
