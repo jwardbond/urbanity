c8bd3b7b7436de5fce1ffd2182592d73da8e62ff
jwardbond <jessewb.c@gmail.com>
2024-06-07 15:50:05 -0400

    Added generic OSM download

--- urbanity/download.py
+++ urbanity/download.py
@@ -3,12 +3,13 @@ import argparse
 import warnings
 from pathlib import Path, PurePath
 
+import yaml
 import shapely
 import osmnx as ox
 import geopandas as gpd
+from matplotlib import pyplot as plt
 
 import utils
-
 
 # TODO make sure coordinates line up
 # TODO Handle missing boundaries (download square box)
@@ -18,7 +19,7 @@ import utils
 def download_osm_boundary(query: str, savepath: PurePath = None):
     """Downloads the bounding polygon for the queried region using open streetmaps
 
-    The coordinate system is WGS84 / EPSG:4326.
+    The coordinate system is WGS84 / EPSG:4326. For more information see `the osmnx docs <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.geocoder.geocode_to_gdf>`
 
     Args:
         query (str): The region to geocode to get the bounding polygon. Can be place name or address]
@@ -49,11 +50,11 @@ def download_osm_network(
 ):
     """Download the street network within some polygon boundary
 
-    The coordinate system is WGS84 / EPSG:4326
+    The coordinate system is WGS84 / EPSG:4326. For more information see `the osmnx docs <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.graph.graph_from_polygon>`
 
     Args:
         polygon (PurePath, shapely.Polygon): The path to the saved polygon, or the polygon itself.
-        savepath (pathlib.PurePath, optional): Save location for downloaded polygon. Defaults to None (not saving)
+        savepath (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
 
     Returns:
         geopandas: The road network as a geojson list of edges in WGS84 / EPSG:4326
@@ -81,33 +82,83 @@ def download_osm_network(
 def download_osm_buildings(
     polygon: PurePath | shapely.Polygon, savepath: PurePath = None
 ):
+    """Download the building polygons from open street maps within some polygon boundary
+
+    The coordinate system is WGS84 / EPSG:4326. For more information see `the osmnx docs <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.features.features_from_polygon>`.
+
+    Args:
+        polygon (PurePath, shapely.Polygon): The path to the saved polygon, or the polygon itself.
+        savepath (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
+
+    Returns:
+        geopandas: A geodataframe of all buildings within the provided boundary in WGS84 / EPSG:4326
+    """
+
     # Parse polygon if required
     polygon = _parse_polygon(polygon)
 
-    # Get buildings from
+    # Get buildings polygons from OSM
     print("Downloading buildings from OSM...", end=" ")
 
     with warnings.catch_warnings():  # HACK geopandas warning suppression
         warnings.simplefilter("ignore")
-        feats = ox.features_from_polygon(polygon, tags={"building": True})
+        buildings = ox.features_from_polygon(polygon, tags={"building": True})
 
     print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
 
     # Save and return
     if savepath:
-        filepath = savepath / (savepath.stem + "osm_building_polygons.geojson")
-        filepath.write_text(feats.to_json())
+        filepath = savepath / (savepath.stem + "_osm_buildings.geojson")
+        filepath.write_text(buildings.to_json())
 
-    print(feats.head(3))
-
-    return feats
+    return buildings
 
 
-def download_rsi(boundary_path: PurePath):  # TODO
+def download_osm_generic(
+    polygon: PurePath | shapely.Polygon,
+    tags: dict,
+    savepath: PurePath = None,
+    savename: str = "custom",
+):
+    """Download generic features from OSMwithin some polygon boundary
+
+    The coordinate system is WGS84 / EPSG:4326. For more information see `the osmnx docs <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.features.features_from_polygon>`.
+
+    Args:
+        polygon (PurePath, shapely.Polygon): The path to the saved polygon, or the polygon itself.
+        tags (dict): A dict of tag-value combinations. See `here <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.features.features_from_polygon>` for more details on format.
+        savepath (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
+        savename (str): name to append to saved geojson
+
+    Returns:
+        geopandas: A geodataframe of all features within the provided boundary in WGS84 / EPSG:4326
+    """
+
+    # Parse polygon if required
+    polygon = _parse_polygon(polygon)
+
+    # Download from osm
+    print(f"Downloading {tags} from OSM...", end=" ")
+
+    with warnings.catch_warnings():  # HACK geopandas warning suppression
+        warnings.simplefilter("ignore")
+        gdf = ox.features_from_polygon(polygon, tags=tags)
+
+    print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
+
+    # Save and return
+    if savepath:
+        filepath = savepath / (savepath.stem + f"_osm_{savename}.geojson")
+        filepath.write_text(gdf.to_json())
+
+    return gdf
+
+
+def download_rsi(boundary_path: PurePath):  # TODO complete function
     pass
 
 
-def download_buildings(boundary_path: PurePath):  # TODO
+def download_buildings(boundary_path: PurePath):  # TODO complete function
     pass
 
 
@@ -119,7 +170,9 @@ def _place_to_filename(place: str):
     return place
 
 
-def _parse_polygon(polygon: PurePath | shapely.Polygon):
+def _parse_polygon(
+    polygon: PurePath | shapely.Polygon,
+):  # TODO not sure I like this function.
     """Parses polygon representations to return a shapely polygon"""
 
     if isinstance(polygon, PurePath):
@@ -130,28 +183,37 @@ def _parse_polygon(polygon: PurePath | shapely.Polygon):
     return polygon
 
 
-if __name__ == "__mains__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("place")
-    args = parser.parse_args()
-    place = args.place
+# if __name__ == "__mains__":
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("place")
+#     args = parser.parse_args()
+#     place = args.place
 
-    savepath = Path(f"results/{_place_to_filename(place)}")
+#     savepath = Path(f"results/{_place_to_filename(place)}")
 
-    savepath.mkdir(exist_ok=True, parents=True)
+#     savepath.mkdir(exist_ok=True, parents=True)
 
-    print(savepath / (savepath.stem + "_filename"))
-    download_osm_boundary(query=place, savepath=savepath)
+#     print(savepath / (savepath.stem + "_filename"))
+#     download_osm_boundary(query=place, savepath=savepath)
 
-    download_osm_network(
-        polygon=savepath / (savepath.stem + "_boundary.geojson"),
-        savepath=savepath,
-    )
+#     download_osm_network(
+#         polygon=savepath / (savepath.stem + "_boundary.geojson"),
+#         savepath=savepath,
+#     )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("boundarypath")
+    parser.add_argument
     args = parser.parse_args()
     boundarypath = Path(args.boundarypath)
 
-    download_osm_buildings(boundarypath, boundarypath.parents[0])
+    with open(Path("./data/temp_tags.yml")) as f:
+        tags = yaml.safe_load(f)
+
+    download_osm_generic(
+        polygon=boundarypath,
+        tags=tags,
+        savepath=boundarypath.parents[0],
+        savename="buildings_parks",
+    )
