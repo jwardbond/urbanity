5963faab0473cd573bdb763d2ddb00a000eee66a
jwardbond <jessewb.c@gmail.com>
2024-06-18 14:31:44 -0400

    Added methods used to parse inputs, hide print statements, save files

--- utils.py
+++ utils.py
@@ -1,3 +1,11 @@
+import os
+import sys
+import pathlib
+import numpy as np
+import geopandas as gpd
+from pathlib import PurePath
+
+
 class PrintColors:
     HEADER = "\033[95m"
     OKBLUE = "\033[94m"
@@ -8,3 +16,50 @@ class PrintColors:
     ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
+
+
+def geojson_to_dataframe(path: pathlib.PurePath):
+    pass
+
+
+def input_to_geodf(
+    x: gpd.geodataframe.GeoDataFrame | gpd.geoseries.GeoSeries | PurePath,
+):
+    """Util function used to parse function inputs."""
+    if isinstance(x, PurePath):
+        x = gpd.read_file(x)
+        x.set_crs("EPSG:4326", allow_override=False)
+        x["id"] = x["id"].astype(str).astype(np.int64)  # since id loads as an object
+    elif not (
+        isinstance(x, gpd.geodataframe.GeoDataFrame)
+        or isinstance(x, gpd.geoseries.GeoSeries)
+    ):
+        raise TypeError(f"Expected geodataframe or path to geojson, got {type(x)}")
+
+    return x
+
+
+class HiddenPrints:
+    """Used with `with` to hide print messages"""
+
+    def __enter__(self):
+        self._origina_stdout = sys.stdout
+        sys.stdout = open(os.devnull, "w")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._origina_stdout
+
+
+def save_geodf_with_prompt(x: gpd.GeoDataFrame, savepath: PurePath):
+    """Util function to save a geodf to geojson with a prompt"""
+    if savepath.exists():
+        prompt_success = False
+        while not prompt_success:
+            overwrite = str(input(f"Overwriting {savepath}. Proceed? (Y/N)"))
+            if overwrite == "Y" or overwrite == "y":
+                prompt_success = True
+                savepath.write_text(x.to_json())
+            elif overwrite == "N" or overwrite == "n":
+                prompt_success = True
+                sys.exit("Exiting")
