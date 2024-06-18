fd7a8b87dc0e7021f76d33c1c10d7e24082e8a04
jwardbond <jessewb.c@gmail.com>
2024-06-18 14:32:42 -0400

    Created tests for segments methods

---
+++ tests/test_segment.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import unittest
+import warnings
+from pathlib import Path
+
+from geopandas.testing import assert_geodataframe_equal
+
+import urbanity.segment as sg
+import utils
+
+os.environ["GDAL_DATA"] = os.path.join(
+    f"{os.sep}".join(sys.executable.split(os.sep)[:-1]), "Library", "share", "gdal"
+)  # HACK GDAL warning suppression
+
+
+class TestSegment(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        # Set output path and get rid of existing files
+        cls.segmentpath = Path("./tests/test_files/test_files_segments.geojson")
+        cls.segmentpath.unlink(missing_ok=True)
+
+        cls.networkpath = Path("./tests/test_files/test_files_road_network.geojson")
+
+    def setUp(self) -> None:
+        warnings.simplefilter(
+            "ignore", category=DeprecationWarning
+        )  # HACK geopandas warning suppression
+
+    def test_segment(self) -> None:
+        segmentpath = self.segmentpath
+        networkpath = self.networkpath
+
+        with utils.HiddenPrints():
+            segments = sg.segment(networkpath, savepath=segmentpath.parent)
+
+        # test that file was created
+        self.assertTrue(segmentpath.exists())
+
+        # test that the output is in the right crs
+        self.assertTrue("EPSG:4326", segments.crs)
+
+        # test that ouputs match
+        segments_from_file = utils.input_to_geodf(segmentpath)
+        assert_geodataframe_equal(segments, segments_from_file)
