1e04aa1c6e354282bf11b286fb08c7e70d78c8b5
jwardbond <jessewb.c@gmail.com>
2024-06-19 18:50:39 -0400

    Added code to download from ms buildings

--- urbanity/download.py
+++ urbanity/download.py
@@ -1,29 +1,35 @@
-import re
+import os
 import argparse
 import warnings
+import tempfile
 from pathlib import Path, PurePath
 
 import yaml
 import shapely
 import osmnx as ox
 import numpy as np
+import pandas as pd
 import geopandas as gpd
 
 import utils
 
-# TODO make sure coordinates line up
+# The following imports are all for microsoft building footprints
+# import planetary_computer
+# import pystac_client
+# import deltalake
+import mercantile
+
 # TODO Handle missing boundaries (download square box)
-# TODO Query resolving, making sure they are all handled the same
 
 
-def download_osm_boundary(query: str, savepath: PurePath = None):
+def download_osm_boundary(query: str, savefolder: PurePath = None):
     """Downloads the bounding polygon for the queried region using open streetmaps
 
     The coordinate system is WGS84 / EPSG:4326. For more information see `the osmnx docs <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.geocoder.geocode_to_gdf>`
 
     Args:
         query (str): The region to geocode to get the bounding polygon. Can be place name or address]
-        savepath (pathlib.PurePath, optional): Save folder for downloaded polygon. Defaults to None (not saving)
+        savefolder (pathlib.PurePath, optional): Save folder for downloaded polygon. Defaults to None (not saving)
 
     Returns:
         shapely.geometry.polygon.Polygon: A shapely polygon representing the boundary of the queried region in EPSG:4326
@@ -34,34 +40,35 @@ def download_osm_boundary(query: str, savepath: PurePath = None):
 
     polygon = gdf_place["geometry"].unary_union
 
-    if savepath:
-        filepath = savepath / (savepath.stem + "_boundary.geojson")
-        filepath.write_text(shapely.to_geojson(polygon))
+    if savefolder:
+        savefolder.mkdir(exist_ok=True, parents=True)
+        savepath = savefolder / (savefolder.stem + "_boundary.geojson")
+        savepath.write_text(shapely.to_geojson(polygon))
 
     return polygon
 
 
 def download_osm_network(
-    polygon: PurePath | shapely.Polygon, savepath: PurePath = None
+    boundary: PurePath | shapely.Polygon, savefolder: PurePath = None
 ):
     """Download the street network within some polygon boundary
 
     The coordinate system is WGS84 / EPSG:4326. For more information see `the osmnx docs <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.graph.graph_from_polygon>`
 
     Args:
-        polygon (PurePath, shapely.Polygon): The path to the saved polygon, or the polygon itself.
-        savepath (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
+        boundary (PurePath, shapely.Polygon): The path to the saved boundary, or the boundary as a shapely polygon.
+        savefolder (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
 
     Returns:
         geopandas.GeoDataframe: The road network as a geojson list of edges in WGS84 / EPSG:4326
     """
 
     # Parse polygon if required
-    polygon = _parse_polygon(polygon)
+    boundary = _parse_polygon(boundary)
 
     # Get graph from OSM
     print("Downloading road network from OSM...", end=" ")
-    G = ox.graph_from_polygon(polygon, network_type="drive", simplify=False)
+    G = ox.graph_from_polygon(boundary, network_type="drive", simplify=False)
     print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
 
     M = ox.convert.to_undirected(G)
@@ -73,8 +80,8 @@ def download_osm_network(
     network = network[["geometry"]]
 
     # Save and return
-    if savepath:
-        savepath = savepath / (savepath.stem + "_road_network.geojson")
+    if savefolder:
+        savepath = savefolder / (savefolder.stem + "_road_network.geojson")
         utils.save_geodf_with_prompt(network, savepath)
 
     network.insert(loc=0, column="id", value=network.index)
@@ -83,28 +90,28 @@ def download_osm_network(
 
 
 def download_osm_buildings(
-    polygon: PurePath | shapely.Polygon, savepath: PurePath = None
+    boundary: PurePath | shapely.Polygon, savefolder: PurePath = None
 ):
     """Download the building polygons from open street maps within some polygon boundary
 
     The coordinate system is WGS84 / EPSG:4326. For more information see `the osmnx docs <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.features.features_from_polygon>`.
 
     Args:
-        polygon (PurePath, shapely.Polygon): The path to the saved polygon, or the polygon itself.
-        savepath (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
+        boundary (PurePath, shapely.Polygon): The path to the saved boundary, or the boundary as a shapely polygon.
+        savefolder (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
 
     Returns:
         geopandas.GeoDataframe: A geodataframe of all buildings within the provided boundary in WGS84 / EPSG:4326
     """
 
     # Parse polygon if required
-    polygon = _parse_polygon(polygon)
+    boundary = _parse_polygon(boundary)
 
     # Get buildings polygons from OSM
     print("Downloading buildings from OSM...", end=" ")
     with warnings.catch_warnings():  # HACK geopandas warning suppression
         warnings.simplefilter("ignore")
-        buildings = ox.features_from_polygon(polygon, tags={"building": True})
+        buildings = ox.features_from_polygon(boundary, tags={"building": True})
     print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
 
     # Correctly format output
@@ -122,8 +129,8 @@ def download_osm_buildings(
     ]
 
     # Save and return
-    if savepath:
-        savepath = savepath / (savepath.stem + "_osm_buildings.geojson")
+    if savefolder:
+        savepath = savefolder / (savefolder.stem + "_osm_buildings.geojson")
         utils.save_geodf_with_prompt(buildings, savepath)
 
     buildings.insert(loc=0, column="id", value=buildings.index)
@@ -132,9 +139,9 @@ def download_osm_buildings(
 
 
 def download_osm_generic(
-    polygon: PurePath | shapely.Polygon,
+    boundary: PurePath | shapely.Polygon,
     tags: dict,
-    savepath: PurePath = None,
+    savefolder: PurePath = None,
     savename: str = "custom",
 ):
     """Download generic features from OSMwithin some polygon boundary
@@ -142,9 +149,9 @@ def download_osm_generic(
     The coordinate system is WGS84 / EPSG:4326. For more information see `the osmnx docs <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.features.features_from_polygon>`.
 
     Args:
-        polygon (PurePath, shapely.Polygon): The path to the saved polygon, or the polygon itself.
+        boundary (PurePath, shapely.Polygon): The path to the saved boundary polygon, or the polygon itself.
         tags (dict): A dict of tag-value combinations. See `here <https://osmnx.readthedocs.io/en/stable/user-reference.html#osmnx.features.features_from_polygon>` for more details on format.
-        savepath (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
+        savefolder (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
         savename (str): name to append to saved geojson
 
     Returns:
@@ -152,27 +159,31 @@ def download_osm_generic(
     """
 
     # Parse polygon if required
-    polygon = _parse_polygon(polygon)
+    boundary = _parse_polygon(boundary)
 
     # Download from osm
     print(f"Downloading {tags} from OSM...", end=" ")
 
     with warnings.catch_warnings():  # HACK geopandas warning suppression
         warnings.simplefilter("ignore")
-        gdf = ox.features_from_polygon(polygon, tags=tags)
+        gdf = ox.features_from_polygon(boundary, tags=tags)
 
     print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
 
     # Remove any point geometries
     gdf = gdf[gdf["geometry"].geom_type != "Point"]
 
+    # Output formatting
     gdf = gdf.reset_index()
+    gdf.index = gdf.index.astype(np.int64)
+    # TODO prune columns
 
     # Save and return
-    if savepath:
-        savepath = savepath / (savepath.stem + f"_osm_{savename}.geojson")
+    if savefolder:
+        savepath = savefolder / (savefolder.stem + f"_osm_{savename}.geojson")
         utils.save_geodf_with_prompt(gdf, savepath)
 
+    gdf.insert(loc=0, column="id", value=gdf.index)
     return gdf
 
 
@@ -180,16 +191,85 @@ def download_rsi(boundary_path: PurePath):  # TODO complete function
     pass
 
 
-def download_buildings(boundary_path: PurePath):  # TODO complete function
-    pass
+def download_ms_buildings(
+    boundary: PurePath | shapely.Polygon,
+    savefolder: PurePath = None,
+):
+    """Download the building polygon data within a given boundary
 
+    The coordinate system is WGS84 / EPSG:4326.
+    Most of the code is adapted from `here <https://github.com/microsoft/GlobalMLBuildingFootprints/blob/main/examples/example_building_footprints.ipynb>`.
 
-def _place_to_filename(place: str):
-    """Given a string, strip all punctuation, lowercase, and replaces spaces with underscores"""
-    place = place.lower()
-    place = re.sub(r"[^\w\s]", "", place)
-    place = place.replace(" ", "_")
-    return place
+    Args:
+        boundary (PurePath, shapely.Polygon): The path to the saved boundary, or the boundary as a shapely polygon.
+        savefolder (pathlib.PurePath, optional): Save location for downloaded polygons. Defaults to None (not saving)
+
+    Returns:
+    """
+
+    # Parse polygon if required
+    boundary = _parse_polygon(boundary)
+
+    # TODO verify boundary is in the correct crs
+
+    # Set up dataset
+    quad_keys = [
+        int(mercantile.quadkey(tile))
+        for tile in mercantile.tiles(*boundary.bounds, zooms=9)
+    ]
+
+    df = pd.read_csv(
+        "https://minedbuildings.blob.core.windows.net/global-buildings/dataset-links.csv"
+    )
+
+    # Load tiles to temp dir
+    idx = 0
+    buildings = gpd.GeoDataFrame()
+
+    print("Downloading buildings from ms-buildings...", end=" ")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # Download the GeoJSON files for each tile that intersects the input geometry
+        tmp_fns = []
+        for quad_key in quad_keys:
+            rows = df[df["QuadKey"] == quad_key]
+            if rows.shape[0] == 1:
+                url = rows.iloc[0]["Url"]
+
+                df2 = pd.read_json(url, lines=True)
+                df2["geometry"] = df2["geometry"].apply(shapely.geometry.shape)
+
+                gdf = gpd.GeoDataFrame(df2, crs=4326)
+                fn = os.path.join(tmpdir, f"{quad_key}.geojson")
+                tmp_fns.append(fn)
+                if not os.path.exists(fn):
+                    gdf.to_file(fn, driver="GeoJSON")
+            elif rows.shape[0] > 1:
+                raise ValueError(f"Multiple rows found for QuadKey: {quad_key}")
+            else:
+                raise ValueError(f"QuadKey not found in dataset: {quad_key}")
+        print(utils.PrintColors.OKGREEN + "Done" + utils.PrintColors.ENDC)
+
+        # Merge the GeoJSON files into a single file
+        for fn in tmp_fns:
+            gdf = gpd.read_file(fn)  # Read each file into a GeoDataFrame
+            gdf = gdf[gdf.geometry.within(boundary)]  # Filter geometries within the AOI
+            gdf["id"] = range(idx, idx + len(gdf))  # Update 'id' based on idx
+            idx += len(gdf)
+            buildings = pd.concat([buildings, gdf], ignore_index=True)
+
+    # Format output
+    buildings = buildings.clip(boundary)
+
+    buildings.reset_index()
+    buildings.index = gdf.index.astype(np.int64)
+
+    if savefolder:
+        savepath = savefolder / (savefolder.stem + "_ms_buildings.geojson")
+        utils.save_geodf_with_prompt(gdf, savepath)
+
+    buildings.insert(loc=0, column="id", value=buildings.index)
+
+    return buildings
 
 
 def _parse_polygon(
@@ -233,9 +313,6 @@ if __name__ == "__main__":
     with open(Path("./data/temp_tags.yml")) as f:
         tags = yaml.safe_load(f)
 
-    download_osm_generic(
-        polygon=boundarypath,
-        tags=tags,
-        savepath=boundarypath.parents[0],
-        savename="buildings_parks",
+    download_ms_buildings(
+        boundary=boundarypath,
     )
