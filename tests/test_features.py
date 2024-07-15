1d7408bae2f6aefe86b0d9fe2a1ef4dcd80a6542
jwardbond <jessewb.c@gmail.com>
2024-07-15 11:13:13 -0400

    Created new tests

---
+++ tests/test_features.py
@@ -0,0 +1,11 @@
+import unittest
+
+import urbanity.features as ft
+
+
+class TestFeatures(unittest.TestCase):
+    def test_add_osm_coverage_features(self):
+        pass
+
+    # TODO test crs
+    # TODO test output format
