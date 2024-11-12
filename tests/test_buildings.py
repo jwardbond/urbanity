6b597091e45417c74dfcfe7289d32d223c9bc58a
jwardbond <jessewb.c@gmail.com>
2024-11-12 18:40:05 -0500

    Added building class

---
+++ tests/test_buildings.py
@@ -0,0 +1,117 @@
+import os
+import sys
+import copy
+import unittest
+import warnings
+from pathlib import Path
+
+
+import shapely
+import geopandas as gpd
+
+from urbanity import Buildings
+from geopandas.testing import assert_geodataframe_equal
+
+
+os.environ["GDAL_DATA"] = os.path.join(
+    f"{os.sep}".join(sys.executable.split(os.sep)[:-1]), "Library", "share", "gdal"
+)  # HACK GDAL warning suppression
+
+
+class TestBuildings(unittest.TestCase):
+    def setUp(self) -> None:
+        warnings.simplefilter(
+            "ignore", category=DeprecationWarning
+        )  # HACK geopandas warning suppression
+
+        # Create a mock region
+
+        building_data = {
+            "id": [0, 1, 2, 3],
+            "height": [10.0, 20.0, 12.0, 25.0],
+            "geometry": [
+                shapely.Polygon([(0, 0), (5, 0), (5, 10), (0, 10)]),  # A=50, V=500
+                shapely.Polygon([(0, 0), (8, 0), (8, 10), (0, 10)]),  # A=80, V=1600
+                shapely.Polygon([(0, 0), (6, 0), (6, 10), (0, 10)]),  # A=60, V=720
+                shapely.Polygon([(0, 0), (9, 0), (9, 10), (0, 10)]),  # A=90, V=2250
+            ],
+        }
+
+        self.proj_crs = "EPSG:3347"
+        self.data = gpd.GeoDataFrame(building_data, crs=self.proj_crs)
+        self.buildings = Buildings(data=self.data, proj_crs=self.proj_crs)
+
+    def test_init(self) -> None:
+        buildings = Buildings(data=self.data, proj_crs=self.proj_crs)
+
+        self.assertTrue("area" in buildings.data)
+        self.assertTrue("id" in buildings.data)
+
+        # Area calcs should be correct
+        self.assertAlmostEqual(90, buildings.data.iloc[3]["area"])
+
+    def test_read_geojson(self) -> None:
+        saved = Buildings.read_geojson(
+            Path("./tests/test_files/test_files_mock_buildings.geojson"),
+            proj_crs=self.proj_crs,
+        )
+
+        new = Buildings(data=self.data, proj_crs=self.proj_crs)
+
+        # Saved and new dataframe should be the same (check to make sure you haven't changed the new dataframe)
+        assert_geodataframe_equal(saved.data, new.data)
+
+        # They should both have the same projected crs system
+        self.assertEqual(saved.proj_crs, new.proj_crs)
+
+    def test_create_volume_flag(self) -> None:
+        b = self.buildings
+        buildings = b.create_volume_flag(min_vol=600, max_vol=2000, flag_name="sfh")
+
+        # The column should be added
+        self.assertTrue("sfh" in buildings.data)
+
+        # Correct buildings are flagged
+        self.assertFalse(buildings.data.iloc[0]["sfh"])
+        self.assertTrue(buildings.data.iloc[1]["sfh"])
+        self.assertTrue(buildings.data.iloc[2]["sfh"])
+        self.assertFalse(buildings.data.iloc[3]["sfh"])
+
+    def test_calc_floors_no_breakpoints(self) -> None:
+        buildings = self.buildings
+
+        buildings = buildings.create_volume_flag(
+            min_vol=600, max_vol=2000, flag_name="sfh"
+        )
+        buildings = buildings.calc_floors(floor_height=5, type_col="sfh")
+
+        # Should have a floors column
+        self.assertTrue("floors" in buildings.data)
+
+        # Non-sfh should have 0 floors
+        self.assertEqual(buildings.data.iloc[0]["floors"], 0)
+        self.assertEqual(buildings.data.iloc[3]["floors"], 0)
+
+        # Building with height exactly equal to 4 * floor height should be four floors (not 5)
+        self.assertEqual(buildings.data.iloc[1]["floors"], 4)
+
+        # Building with height greater than 2 * floor height should be three floors
+        self.assertEqual(buildings.data.iloc[2]["floors"], 3)
+
+    def test_calc_floors_breakpoints(self) -> None:
+        buildings = self.buildings
+
+        buildings = buildings.calc_floors(
+            floor_height=5,
+            floor_breakpoints=[10, 20],
+        )
+        # Should have a floors column
+        self.assertTrue("floors" in buildings.data)
+
+        # Building with height on breakpoint should not have an extra floor
+        self.assertEqual(buildings.data.iloc[0]["floors"], 1)
+        self.assertEqual(buildings.data.iloc[1]["floors"], 2)
+
+        # Building with height greater than 10m breakpoint should be 2 floors
+        self.assertEqual(buildings.data.iloc[2]["floors"], 2)
+        self.assertEqual(buildings.data.iloc[3]["floors"], 3)
---
+++ urbanity/buildings.py
@@ -0,0 +1,117 @@
+import pathlib
+from typing import Self
+
+import numpy as np
+import geopandas as gpd
+
+
+class Buildings:
+    def __init__(self, data: gpd.GeoDataFrame, proj_crs: str):
+        if "area" not in data:
+            data["area"] = data.to_crs(proj_crs)["geometry"].area
+        if "id" not in data:
+            data.insert(loc=0, column="id", value=range(len(data)))
+
+        data = data.to_crs("EPSG:4326")  # default crs
+
+        self.data = data
+        self.proj_crs = proj_crs
+
+    @classmethod
+    def read_geojson(cls, data: pathlib.PurePath, proj_crs: str):
+        data = gpd.read_file(data)
+        data.set_crs("EPSG:4326", allow_override=False)
+        data["id"] = (
+            data["id"].astype(str).astype(np.int64)
+        )  # since id loads as an object
+        data = data.fillna(value=np.nan)
+
+        return cls(data, proj_crs)
+
+    def create_volume_flag(
+        self, min_vol: float, max_vol: float, flag_name: str
+    ) -> Self:
+        """Selects buildings within a given volume range
+
+        Args:
+            min_vol (float): Minimum volume for filtering in cubic meters. (m^3)
+            max_vol (float): Maximum volume for filtering in cubic meters. (m^3)
+            flag_name (str): The name for the building type (e.g. "sfh")
+
+        Returns:
+            object: A copy of the object with an updated `buildings` attribute, containing:
+                - 'volume': Volume per building.
+                - '[flag_name]': Boolean indicating if volume is within range.
+
+        Raises:
+            AttributeError: If there is no height information in the building data
+        """
+        data = self.data
+
+        if "height" not in data:
+            raise AttributeError('building data does not contain a "height" column')
+
+        if "volume" not in data:
+            data["volume"] = data["area"] * data["height"]
+
+        # Filtering by volume
+        data[flag_name] = data["volume"].map(
+            lambda x: (x >= min_vol) and (x <= max_vol)
+        )
+
+        return Buildings(data, self.proj_crs)
+
+    def calc_floors(
+        self,
+        floor_height: float = 2.75,
+        floor_breakpoints: list[float] = None,
+        type_col: str = None,
+    ):
+        """Filters buildings by volume and assigns floor counts based on height.
+
+        Args:
+            floor_height (float): Height per floor in meters. Defaults to 2.75.
+            floor_breakpoints (list[float], optional): Custom height breakpoints for floors, in meters. Defaults to None (will construct from floor height).
+            type_col (str): The name for the building type. Defaults to "sfh" (single family home)
+
+        Returns:
+            object: A copy of the object with an updated `buildings` attribute, containing:
+                - 'floors': Estimated floor count based on height and breakpoints or floor height.
+
+        Raises:
+            AttributeError: If the `buildings` attribute is not set.
+        """
+        data = self.data
+
+        # Determining Floors
+        breakpoints = [-1, 0]
+        max_height = data["height"].max()
+
+        if floor_breakpoints is not None:
+            breakpoints = breakpoints + floor_breakpoints
+
+        breakpoints = breakpoints + list(
+            np.arange(
+                breakpoints[-1] + floor_height, max_height + floor_height, floor_height
+            )
+        )
+
+        breakpoints = np.array(breakpoints)
+
+        if type_col is not None:
+            data["floors"] = data.apply(
+                lambda r: np.nanargmax(
+                    np.where(breakpoints < r.height, breakpoints, np.nan)
+                )
+                if r[type_col]
+                else 0,
+                axis=1,
+            )
+        else:
+            data["floors"] = data["height"].map(
+                lambda h: np.nanargmax(np.where(breakpoints < h, breakpoints, np.nan))
+            )
+
+        data["floors"] = data["floors"].astype(int)
+
+        return Buildings(data, self.proj_crs)
