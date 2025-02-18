a04af2c0b0c3f267d28bb4606bb9fd356c5e50fd
Jesse Ward-Bond <jessewb.c@gmail.com>
2025-02-18 18:23:47 -0500

    Created this to generate graphs from intersection or overlapping polygons

---
+++ tests/test_polygraph.py
@@ -0,0 +1,169 @@
+import unittest
+import warnings
+
+import geopandas as gpd
+import shapely
+
+from urbanity.polygraph import PolyGraph
+
+
+class TestPolygraphInitIntersectsPredicate(unittest.TestCase):
+    def setUp(self):
+        warnings.simplefilter(
+            "ignore",
+            category=DeprecationWarning,
+        )  # HACK geopandas warning suppression
+
+    def test_create_from_three_overlapping_polygons(self):
+        # Create three overlapping squares
+        poly1 = shapely.Polygon([(0, 0), (2, 0), (2, 2), (0, 2)])
+        poly2 = shapely.Polygon([(1, 1), (3, 1), (3, 3), (1, 3)])
+        poly3 = shapely.Polygon([(2, 2), (4, 2), (4, 4), (2, 4)])
+
+        gs = gpd.GeoSeries([poly1, poly2, poly3])
+        graph = PolyGraph.create_from_geoseries(gs)
+
+        # Each polygon should intersect with the other two
+        self.assertEqual(graph.adj_list[0], {1, 2})
+        self.assertEqual(graph.adj_list[1], {0, 2})
+        self.assertEqual(graph.adj_list[2], {0, 1})
+
+    def test_create_from_touching(self):
+        # Create three overlapping squares
+        poly1 = shapely.Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])  # edge with 1
+        poly2 = shapely.Polygon([(1, 0), (2, 0), (2, 1), (1, 1)])
+        poly3 = shapely.Polygon([(2, 1), (3, 1), (3, 2), (2, 2)])  # corner with 1
+
+        gs = gpd.GeoSeries([poly1, poly2, poly3])
+        graph = PolyGraph.create_from_geoseries(gs)
+
+        # Each polygon should intersect with the other two
+        self.assertEqual(graph.adj_list[0], {1})
+        self.assertEqual(graph.adj_list[1], {0, 2})
+        self.assertEqual(graph.adj_list[2], {1})
+
+    def test_create_from_transitive_overlapping_polygons(self):
+        # Create three squares where only adjacent ones overlap
+        poly1 = shapely.Polygon([(0, 0), (2, 0), (2, 2), (0, 2)])
+        poly2 = shapely.Polygon([(2, 0), (4, 0), (4, 2), (2, 2)])
+        poly3 = shapely.Polygon([(4, 0), (6, 0), (6, 2), (4, 2)])
+
+        gs = gpd.GeoSeries([poly1, poly2, poly3])
+        graph = PolyGraph.create_from_geoseries(gs)
+
+        # First polygon only intersects second
+        self.assertEqual(graph.adj_list[0], {1})
+        # Middle polygon intersects both
+        self.assertEqual(graph.adj_list[1], {0, 2})
+        # Last polygon only intersects middle
+        self.assertEqual(graph.adj_list[2], {1})
+
+    def test_create_from_empty_geoseries(self):
+        gs = gpd.GeoSeries([])
+        graph = PolyGraph.create_from_geoseries(gs)
+        self.assertEqual(graph.adj_list, {})
+
+
+class TestPolygraphInitOverlapsPredicate(unittest.TestCase):
+    def setUp(self):
+        warnings.simplefilter(
+            "ignore",
+            category=DeprecationWarning,
+        )  # HACK geopandas warning suppression
+
+    def test_create_from_three_overlapping_polygons(self):
+        # Create three overlapping squares
+        poly1 = shapely.Polygon([(0, 0), (2, 0), (2, 2), (0, 2)])  # overlaps 1
+        poly2 = shapely.Polygon([(1, 1), (3, 1), (3, 3), (1, 3)])  # overlaps both
+        poly3 = shapely.Polygon([(2, 2), (4, 2), (4, 4), (2, 4)])  # overlaps 1
+
+        gs = gpd.GeoSeries([poly1, poly2, poly3])
+        graph = PolyGraph.create_from_geoseries(gs, predicate="overlaps")
+
+        # Each polygon should intersect with the other two
+        self.assertEqual(graph.adj_list[0], {1})
+        self.assertEqual(graph.adj_list[1], {0, 2})
+        self.assertEqual(graph.adj_list[2], {1})
+
+    def test_create_from_touching(self):
+        poly1 = shapely.Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])  # edge with 1
+        poly2 = shapely.Polygon([(1, 0), (2, 0), (2, 1), (1, 1)])
+        poly3 = shapely.Polygon([(2, 1), (3, 1), (3, 2), (2, 2)])  # corner with 1
+
+        gs = gpd.GeoSeries([poly1, poly2, poly3])
+        graph = PolyGraph.create_from_geoseries(gs, predicate="overlaps")
+
+        # No polygons should overlap
+        self.assertEqual(graph.adj_list[0], set())
+        self.assertEqual(graph.adj_list[1], set())
+        self.assertEqual(graph.adj_list[2], set())
+
+    def test_create_from_transitive_overlapping_polygons(self):
+        # Create three squares where only adjacent ones overlap
+        poly1 = shapely.Polygon([(0, 0), (2, 0), (2, 2), (0, 2)])
+        poly2 = shapely.Polygon([(2, 0), (4, 0), (4, 2), (2, 2)])
+        poly3 = shapely.Polygon([(4, 0), (6, 0), (6, 2), (4, 2)])
+
+        gs = gpd.GeoSeries([poly1, poly2, poly3])
+        graph = PolyGraph.create_from_geoseries(gs)
+
+        # First polygon only intersects second
+        self.assertEqual(graph.adj_list[0], {1})
+        # Middle polygon intersects both
+        self.assertEqual(graph.adj_list[1], {0, 2})
+        # Last polygon only intersects middle
+        self.assertEqual(graph.adj_list[2], {1})
+
+    def test_create_from_empty_geoseries(self):
+        gs = gpd.GeoSeries([])
+        graph = PolyGraph.create_from_geoseries(gs)
+        self.assertEqual(graph.adj_list, {})
+
+
+class TestConnectedComponents(unittest.TestCase):
+    def setUp(self):
+        warnings.simplefilter(
+            "ignore",
+            category=DeprecationWarning,
+        )  # HACK geopandas warning suppression
+
+        adj_list = {
+            0: [1, 2],
+            1: [0, 2],
+            2: [0, 1],
+            3: [],
+        }
+
+        self.pg = PolyGraph(adj_list)
+
+    def test_dfs_connected_nodes(self):
+        connected = self.pg.get_connected(0)
+        self.assertEqual(set(connected), {0, 1, 2})
+
+    def test_isolated_root_node(self):
+        connected = self.pg.get_connected(3)
+        self.assertEqual(set(connected), {3})
+
+    def test_invalid_root_node(self):
+        with self.assertRaises(KeyError):
+            self.pg.get_connected(99)
+
+
+class TestCreateCCMap(unittest.TestCase):
+    def test_multiple_components_mapping(self):
+        # Graph with 2 components: [0,1,2] and [3,4]
+        adj_list = {0: [1, 2], 1: [0, 2], 2: [0, 1], 3: [4], 4: [3]}
+        pg = PolyGraph(adj_list)
+        expected = {0: 0, 1: 0, 2: 0, 3: 1, 4: 1}
+        self.assertEqual(pg.create_connected_components_map(), expected)
+
+    def test_empty_graph_mapping(self):
+        pg = PolyGraph(adj_list={})
+        self.assertEqual(pg.create_connected_components_map(), {})
+
+    def test_isolated_nodes_mapping(self):
+        # Graph with 3 isolated nodes
+        adj_list = {0: [], 1: [], 2: []}
+        pg = PolyGraph(adj_list)
+        expected = {0: 0, 1: 1, 2: 2}
+        self.assertEqual(pg.create_connected_components_map(), expected)
